dd545c7e4cd856575c71229a021409de56d4267e
DeoDao <denminstudy@gmail.com>
2020-08-24 17:07:15 +0300

    Day01_task1-6

---
+++ src/Day1/Task3.java
@@ -0,0 +1,10 @@
+package Day1;
+
+public class Task3 {
+    public static void main(String[] args){
+        for(int i = 0; i <=9;i++){
+            System.out.println("JAVA ");
+        }
+    }
+}
+
---
+++ src/Day1/Task6.java
@@ -0,0 +1,25 @@
+package Day1;
+
+public class Task6 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int e = 5;
+        int f = 6;
+        int g = 7;
+        int h = 8;
+        int l = 9;
+        int o = 10;
+        for (int i = 1; i < 10; i++) {
+            for (int k = 7; k < 10; k++) {
+                System.out.printf(k * a + ", " + k * b+ ", " + k * c + ", "+ k * d + ", "+ k * e + ", "+ k * f + ", "+ k * g + ", "+ k * h + ", "+ k * l + ", "+ k * o + ", ");
+                System.out.println();
+            }
+        }
+    }
+}
+//// не успел разобраться
+
+
